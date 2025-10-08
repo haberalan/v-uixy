b7b60402c657a4d97b5f86505a5aa667ff2539b9
Alan Haber <dev@alanhaber.me>
2025-10-08 23:49:20 +0200

    chore: prep 1.1.2

--- bin/v-uixy.js
+++ bin/v-uixy.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
+import { createRequire } from "module";
 import { Command } from "commander";
 import add from "../commands/add.js";
 import remove from "../commands/remove.js";
 import list from "../commands/list.js";
 import init from "../commands/init.js";
 
+const require = createRequire(import.meta.url);
+
+let pkgVersion = "0.0.0";
+try {
+  const pkg = require("../package.json");
+  pkgVersion = pkg.version || pkgVersion;
+} catch {}
+
 const program = new Command();
 
 program
   .name("v-uixy")
   .description("A UI components CLI for Nuxt + Tailwind")
-  .version("1.1.0");
+  .version(pkgVersion);
 
 program.command("add <component>").description("Add a component").action(add);
 
