1529ddb03a0bb3608d1f87c2b316c8cc904ab6d0
Alan Haber <dev@alanhaber.me>
2025-10-11 14:20:20 +0200

    chore: prep 1.1.3

--- templates/nuxt.config.ts
+++ templates/nuxt.config.ts
@@ -1,12 +1,15 @@
 import svgLoader from "vite-svg-loader";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-04-24",
-  css: ["./assets/css/main.css"],
-  modules: ["motion-v/nuxt"],
   devtools: { enabled: true },
+  css: [join(currentDir, "./assets/css/main.css")],
   vite: {
     plugins: [svgLoader(), tailwindcss()],
   },
